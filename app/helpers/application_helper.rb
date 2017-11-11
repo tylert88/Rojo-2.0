c9549c0c1f0686884fdc93c7a3383ef140126ec1
Tyler Torres <tyler.john.torres@gmail.com>
2017-11-10 18:46:27 -0700

    added fb login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def avatar_url(user)
     if user.image
-      "http://graph.facebook.com/#{user.uid}/picture?type=large"
+      "http://graph.facebook.com/#{user.uid}/picture?type=small"
     else
       gravatar_id = Digest::MD5::hexdigest(user.email).downcase
       "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
