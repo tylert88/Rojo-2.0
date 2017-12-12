85791576d85d985da5933c31204edff9762f51cb
Tyler Torres <tyler.john.torres@gmail.com>
2017-12-11 18:35:52 -0700

    Changed Gemfile for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Show full error reports.
+  
   # config.consider_all_requests_local = true
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
