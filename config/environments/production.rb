4bd30822576b35e4e4257ec78b97a2838402d763
Tyler Torres <tyler.john.torres@gmail.com>
2017-12-12 09:25:55 -0700

    Changed Gemfile for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'http://assets.hidden-refuge-93553.herokuapp.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -90,7 +90,7 @@ Rails.application.configure do
   end
 
   # This is the configs for instant notifications
-  config.action_cable.url = "WSS://hidden-refuge-93553.herokuapp.com/cable"
+  config.action_cable.url = "wss://hidden-refuge-93553.herokuapp.com/cable"
 
   # config.action_mailer.default_url_options = {:host => 'hidden-refuge-93553.herokuapp.com', :protocol => 'https'}
   config.action_mailer.default_url_options = { host: 'https://hidden-refuge-93553.herokuapp.com/', protocol: 'http'}
