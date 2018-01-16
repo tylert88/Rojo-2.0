3aeddc510c7c54aea19e1382de6355e23f3e4f5c
Tyler Torres <tyler.john.torres@gmail.com>
2018-01-16 13:58:26 -0700

    gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,3 +122,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+# CHECK HERE FOR POSSIBLY SOLUTION
+# https://devcenter.heroku.com/articles/request-timeout
